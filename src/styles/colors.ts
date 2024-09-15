49d6178717e2b12f2d49c03412c87c41409ad609
JASUN LEE <liswktjs@gmail.com>
2024-09-15 23:15:05 +0900

    style: tailwind css color design set 추가

---
+++ src/styles/colors.ts
@@ -0,0 +1,26 @@
+export const colors = {
+  black_400: "#1C1C1C",
+  black_500: "#000000",
+
+  charcoal_400: "#90A5AD",
+  charcoal_500: "#718188",
+
+  cobalt_50: "#E5EEFF",
+  cobalt_100: "#BFD7FF",
+  cobalt_200: "#99BCFF",
+  cobalt_300: "#739FFF",
+  cobalt_400: "#4A7DF1",
+  cobalt_500: "#325FC7",
+  cobalt_600: "#2B54B0",
+  cobalt_700: "#1F3D89",
+  cobalt_800: "#142960",
+  cobalt_900: "#0B1B45",
+
+  white: "#ffffff",
+
+  red_400: "#e65555",
+  red_500: "#db3b3b",
+
+  green_400: "#5cdb87",
+  green_500: "#27db63",
+};
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+import { colors } from "./src/styles/colors";
+
+module.exports = {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    colors,
+    extend: {},
+  },
+  plugins: [],
+};
